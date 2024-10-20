d32a6c59573abf5abd0fd1583c83f079073b92f4
FerMod <tudelaigarcia@gmail.com>
2024-10-20 16:27:08 +0200

    Modify projectile parameters

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -8,6 +8,8 @@ namespace SpaceGame
         public float force = 1000f;
         public float damage = 25f;
 
+        public float radius = 100f;
+
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
         {
@@ -23,6 +25,11 @@ namespace SpaceGame
             if (collision.gameObject.TryGetComponent(out Health health))
             {
                 health.ChangeHealth(-damage);
+            }
+
+            if(collision.gameObject.TryGetComponent(out Rigidbody rigidbody))
+            {
+                rigidbody.AddExplosionForce(force, transform.position, radius, 0f, ForceMode.Impulse);
             }
 
             Destroy(gameObject);
