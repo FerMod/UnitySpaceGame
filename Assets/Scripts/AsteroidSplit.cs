517703b8ec97691b00f9c3b41034a07320119084
FerMod <tudelaigarcia@gmail.com>
2024-10-20 16:08:53 +0200

    Use hook to listen for health change

--- Assets/Scripts/AsteroidSplit.cs
+++ Assets/Scripts/AsteroidSplit.cs
@@ -4,31 +4,37 @@ namespace SpaceGame
 {
 
     [RequireComponent(typeof(Rigidbody))]
+    [RequireComponent(typeof(Health))]
     public class AsteroidSplit : MonoBehaviour
     {
-
-        public float radius = 100.0f;
-        public float power = 5000.0f;
 
         public float spawnRadius = 1f;
         public int amount = 5;
         public GameObject[] debris = { };
 
+        [Header("Debug")]
         public bool showSpawnRadius = false;
+
+        private Health health;
+
+        [Header("TEST")]
+        public float radius = 100.0f;
+        public float power = 5000.0f;
+
 
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
         {
+            health = GetComponent<Health>();
+            health.OnHealthChanged += OnHealthChanged;
         }
 
-        // Update is called once per frame
-        void Update()
+        private void OnHealthChanged(object source, float oldHealth, float newHealth)
         {
-            if (Input.GetKeyDown(KeyCode.Space))
+            if (newHealth <= 0)
             {
                 Split();
             }
-
         }
 
         void Split()
@@ -44,7 +50,7 @@ namespace SpaceGame
                 // TODO: This logic should be handled on the proyectile logic
                 var rb = instance.GetComponent<Rigidbody>();
                 rb.AddExplosionForce(power, transform.position, spawnRadius * 100f, 0f, ForceMode.Impulse);
-                rb.AddRelativeTorque(new Vector3(Random.Range(-10.0f, 10.0f), Random.Range(-10.0f, 10.0f), Random.Range(-10.0f, 10.0f)));
+                rb.AddRelativeTorque(RandomTorque(-10f, 10f));
                 // rb.angularVelocity = Random.insideUnitSphere * 0.9f;
 
                 Destroy(instance, Random.Range(5f, 8f));
@@ -55,7 +61,16 @@ namespace SpaceGame
         private GameObject SpawnDebris()
         {
             var index = Random.Range(0, debris.Length);
-            return Instantiate(debris[index], transform.position + Random.insideUnitSphere * spawnRadius, Random.rotation);
+            var instance = Instantiate(debris[index], transform.position + Random.insideUnitSphere * spawnRadius, Random.rotation);
+            return instance;
+        }
+
+        private Vector3 RandomTorque(float min, float max)
+        {
+            var x = Random.Range(min, max);
+            var y = Random.Range(min, max);
+            var z = Random.Range(min, max);
+            return new Vector3(x, y, z);
         }
 
 
