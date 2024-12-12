aa2d7e33a974a16fe09f3c2bf5aa12cf3b69f1d0
FerMod <tudelaigarcia@gmail.com>
2024-12-12 18:41:28 +0100

    Improve destruction script

--- Assets/Scripts/SpaceshipDestruction.cs
+++ Assets/Scripts/SpaceshipDestruction.cs
@@ -1,76 +1,117 @@
-using SpaceGame;
+using System.Collections;
 using UnityEngine;
 
-public class SpaceshipDestruction : MonoBehaviour
+namespace SpaceGame
 {
-    public Health health;
-    public Transform spaceship;
-
-    public float explosionForce = 500f;
-    public float explosionRadius = 5f;
-    public Vector3 explosionCenterOffset = Vector3.zero;
-
-    [Header("Effects")]
-    public GameObject bigExplosion;
-    public GameObject mediumExplosion;
-    public GameObject smallExplosion;
-
-    void Start()
+    public class SpaceshipDestruction : MonoBehaviour
     {
-        health.OnNoHealth += OnNoHealth;
-    }
-    private void OnNoHealth(object source, float oldHealth, float newHealth)
-    {
-        Die();
-    }
+        public Plane plane;
 
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
+        /// <summary>
+        /// The parent of all the spaceship parts.
+        /// </summary>
+        public Transform spaceship;
+
+        public float explosionForce = 500f;
+        public float explosionRadius = 5f;
+        public Vector3 explosionCenterOffset = Vector3.zero;
+
+        [Header("Effects")]
+        public GameObject bigExplosion;
+        public GameObject mediumExplosion;
+        public GameObject smallExplosion;
+
+        void Start()
         {
-            health.ChangeHealth(-9999);
-
+            plane.GetComponent<Health>().OnNoHealth += OnNoHealth;
         }
-    }
 
-    public void Die()
-    {
-        PlayEffect(bigExplosion, transform.position, transform.rotation);
-
-        // Unparent all children
-        foreach (Transform child in spaceship.transform)
+        private void OnNoHealth(object source, float oldHealth, float newHealth)
         {
-            // Detach the child from the parent
-            child.SetParent(null);
+            Die();
+        }
 
-            // Add Rigidbody if it doesn't already have one
-            Rigidbody rb = child.gameObject.GetComponent<Rigidbody>();
-            if (rb == null)
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
             {
-                rb = child.gameObject.AddComponent<Rigidbody>();
-            }
-            rb.useGravity = false;
+                plane.GetComponent<Health>().ChangeHealth(-9999);
 
-            rb.AddExplosionForce(explosionForce, explosionCenterOffset, explosionRadius);
-            //rb.AddForce(Random.insideUnitSphere * explosionForce);
-            rb.AddTorque(Random.insideUnitSphere * explosionForce);
+            }
         }
 
-        // Destroy the spaceship itself
-        Destroy(gameObject);
+        public void Die()
+        {
+
+            PlayEffect(bigExplosion, transform.position, transform.rotation);
+
+            //PlayEffectsRandom(1, 4, 0.1f, 1f);
+
+            // Unparent all children
+            var childCount = spaceship.transform.childCount;
+            for (var i = childCount - 1; i >= 0; i--)
+            {
+                var child = spaceship.transform.GetChild(i);
+                // Detach the child from the parent
+                child.SetParent(null);
+
+                // Add Rigidbody if it doesn't already have one
+                var rb = child.gameObject.GetComponent<Rigidbody>();
+                if (rb == null)
+                {
+                    rb = child.gameObject.AddComponent<Rigidbody>();
+                }
+                rb.useGravity = false;
+
+                rb.AddExplosionForce(explosionForce, explosionCenterOffset, explosionRadius);
+                //rb.AddForce(Random.insideUnitSphere * explosionForce);
+                rb.AddTorque(Random.insideUnitSphere * explosionForce);
+            }
+
+            // Destroy the spaceship itself
+            Destroy(plane);
+        }
+
+        private void PlayEffect(GameObject gameObject, Vector3 position, Quaternion rotation)
+        {
+            if (gameObject == null) return;
+
+            var effectInstance = Instantiate(gameObject, position, rotation);
+            effectInstance.TryGetComponent(out ParticleSystem effect);
+            if (effect == null) return;
+
+            effect.Play();
+
+            Destroy(effectInstance, effect.main.duration);
+        }
+        IEnumerator DelayAction(Transform transform, float delayTime)
+        {
+            //Wait for the specified delay time before continuing.
+            yield return new WaitForSeconds(delayTime);
+
+            PlayEffect(smallExplosion, transform.position, transform.rotation);
+        }
+
+
+        private void PlayEffectsRandom(int min, int max, float minDelay, float maxDelay)
+        {
+            StartCoroutine(PlayEffectsRandomCouroutine(min, max, minDelay, maxDelay));
+        }
+
+        private IEnumerator PlayEffectsRandomCouroutine(int min, int max, float minDelay, float maxDelay)
+        {
+            // Generate a random number of times to play the effect.
+            var randomTimes = Random.Range(min, max + 1);
+            for (var i = 0; i < randomTimes; i++)
+            {
+                // Wait for a random delay before playing the effect.
+                var randomDelay = Random.Range(minDelay, maxDelay);
+                yield return new WaitForSeconds(randomDelay);
+
+                // Call the PlayEffect method.
+                PlayEffect(mediumExplosion, transform.position, transform.rotation);
+            }
+        }
+
     }
-
-    private void PlayEffect(GameObject gameObject, Vector3 position, Quaternion rotation)
-    {
-        if (gameObject == null) return;
-
-        var effectInstance = Instantiate(gameObject, position, rotation);
-        effectInstance.TryGetComponent(out ParticleSystem effect);
-        if (effect == null) return;
-
-        effect.Play();
-
-        Destroy(effectInstance, effect.main.duration);
-    }
-
 }
