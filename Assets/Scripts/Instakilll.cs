25c49ea79ea2b789684ff76d86e63e5f0aa045c0
FerMod <tudelaigarcia@gmail.com>
2024-12-18 16:57:10 +0100

    Add damage and kill area

---
+++ Assets/Scripts/Instakilll.cs
@@ -0,0 +1,34 @@
+using SpaceGame;
+using System.Linq;
+using UnityEngine;
+
+public class Instakill : MonoBehaviour
+{
+    [SerializeField, Tooltip("Instakill GameObjects with specified tags")]
+    private string[] tags = { };
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!IsValidDamageTarget(other.gameObject.tag)) return;
+        KillTarget(other);
+    }
+
+    /// <summary>
+    /// Filter by tags if not empty.
+    /// </summary>
+    /// <param name="tag"></param>
+    /// <returns></returns>
+    private bool IsValidDamageTarget(string tag)
+    {
+        // No filters applied, is valid.
+        if (tags.Length <= 0) return true;
+        return tags.Contains(tag);
+    }
+
+    private void KillTarget(Collider other)
+    {
+        other.TryGetComponent(out Health target);
+        if (target == null) return;
+        target.ChangeHealth(-target.CurrentHealth);
+    }
+}
